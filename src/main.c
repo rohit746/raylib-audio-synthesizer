e4cc9d1bdca6735df5d4863ab433bdc638a8b2ad
Rohit Jadhav <rjjadhav746@gmail.com>
2022-12-14 00:04:52 +0530

    Lfo and Mouse Interaction.

--- src/main.c
+++ src/main.c
@@ -9,12 +9,17 @@ typedef struct {
     float phase_stride;
 } Oscillator;
 
+void updateOsc(Oscillator* osc)
+{
+    osc->phase += osc->phase_stride;
+    if(osc->phase >=  1.0f)
+        osc->phase -= 1.0f;
+}
+
 void updateSignal(float* signal, Oscillator* osc)
 {
     for (size_t t = 0; t < STREAM_BUFFER_SIZE; t++) {
-        osc->phase += osc->phase_stride;
-        if(osc->phase >=  1.0f)
-            osc->phase -= 1.0f;
+        updateOsc(osc);
         signal[t] = sinf(2.0f * PI * osc->phase);
     }
 }
@@ -37,18 +42,22 @@ int main()
     float sample_duration = (1.0f/(float)sample_rate);
 
     Oscillator osc = {.phase = 0.0f, .phase_stride = frequency * sample_duration};
-    Oscillator lfo = {.phase = 0.0f, .phase_stride = frequency * sample_duration};
+    Oscillator lfo = {.phase = 0.0f, .phase_stride = 1000.0f * sample_duration};
 
     float signal[STREAM_BUFFER_SIZE];
 
     while (WindowShouldClose() == false)
     {
+        Vector2 mouse_pos = GetMousePosition();
+
         if(IsAudioStreamProcessed(synthStream))
         {
+            updateOsc(&lfo);
+            frequency = 220.0f + (mouse_pos.x / (float)screen_width) * 100.0f;
+            osc.phase_stride = frequency * sample_duration;
+
             updateSignal(signal, &osc);
             UpdateAudioStream(synthStream, signal, STREAM_BUFFER_SIZE);
-            frequency += 0.7f;
-            osc.phase_stride = frequency * sample_duration;
         }
 
         BeginDrawing();
